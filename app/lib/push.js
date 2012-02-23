fa49d59cbfa1e21399c1acc80c440e2d0d9f3b1a
Sam Hon <coffeedaemon@gmail.com>
2012-02-23 17:08:47 +0800

    Path, host, destination, watch interval and delay settings are now configurable. Push will monitor the <path> for changes every <interval> seconds. Every file change will set a timeout in <delay> seconds after clearing all previous timeouts. When the timeout occurs, it checks if all files under path are modified within the same day, if so it will call the send method to start copying the files to destination using SCP.

--- app/lib/push.js
+++ app/lib/push.js
@@ -1,37 +1,52 @@
+require('sugar');
 var fs = require('fs');
 var watchd = require('watch');
+var scp = require('scp');
 
 var push = module.exports = {
     config : {},
-    changed: {},
     timer  : {},
     init: function() {
         var configJSON = fs.readFileSync(APP_ROOT + "/config/settings.json");
-        var config     = JSON.parse(configJSON.toString());
-        push.config    = config;
+        push.config    = JSON.parse(configJSON.toString());
     },
     start: function() {
         push.init();
         push.run();
     },
     run: function() {
-        watchd.createMonitor(push.config.path, function(monitor) {
+        watchd.createMonitor(push.config.path, {interval: push.config.interval}, function(monitor) {
           monitor.on('created', function(f, stat) {
-            push.changed[f] = true;
             clearTimeout(push.timer);
-            push.timer = setTimeout(push.send, 30000);
+            push.timer = setTimeout(push.checkDates, push.config.delay);
           });
           monitor.on('changed', function(f, stat) {
-            push.changed[f] = true;
             clearTimeout(push.timer);
-            push.timer = setTimeout(push.send, 30000);
+            push.timer = setTimeout(push.checkDates, push.config.delay);
           });
           monitor.on('removed', function(f, stat) {
-            push.changed[f] = false;
           });
         });
     },
+    checkDates: function() {
+        fs.readdir(push.config.path, function(err, files) {
+            var dates = [];
+            files.each(function(file) {
+                stat = fs.statSync(push.config.path + file);
+                dates.add(Date.create(stat.mtime).format());
+            });
+            if(dates.unique().count() == 1) {
+                push.send();
+            }
+        });
+    },
     send: function() {
-
+        var options = {
+          file: push.config.path + "*";
+          host: push.config.host,
+          path: push.config.destination
+        }
+        scp.send(options, function (err) {
+        });
     }
 };
